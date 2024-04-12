aaaae0a66dc83332fa8ec3a7bcd326c71dd09454
Sermeus Steven <etu44050@henallux.be>
2024-04-12 09:04:41 +0200

    Navigation and Command template

---
+++ apps/web/src/components/CommandMenu.tsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+import {
+  Calculator,
+  Calendar,
+  CreditCard,
+  Settings,
+  Smile,
+  User,
+} from "lucide-react";
+
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+
+export function CommandMenu() {
+  const [open, setOpen] = React.useState(false);
+
+  React.useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
+  return (
+    <CommandDialog open={open} onOpenChange={setOpen}>
+      <CommandInput placeholder="Type a command or search..." />
+      <CommandList>
+        <CommandEmpty>No results found.</CommandEmpty>
+        <CommandGroup heading="Suggestions">
+          <CommandItem
+            onSelect={() => {
+              setOpen(false);
+            }}
+          >
+            Close Command Menu
+          </CommandItem>
+          <CommandItem>Search Emoji</CommandItem>
+          <CommandItem>Calculator</CommandItem>
+        </CommandGroup>
+      </CommandList>
+    </CommandDialog>
+  );
+}
--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -3,11 +3,12 @@ import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { Inter } from "next/font/google";
 import { ToastContainer } from "react-toastify";
-
+import { Navigation } from "@/components/Navigation";
 import "react-toastify/dist/ReactToastify.css";
 import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
+import { CommandMenu } from "@/components/CommandMenu";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -21,6 +22,8 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <SessionProvider session={session}>
       <main className={`font-sans ${inter.variable}`}>
+        <Navigation />
+        <CommandMenu />
         <ToastContainer />
         <Component {...pageProps} />
       </main>
